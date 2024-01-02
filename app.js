519bd95ef5a6e605b159549175f33ac98652932c
NevzanEmreErcan <88339594+NevzanEmreErcan@users.noreply.github.com>
2024-01-02 09:54:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const deleteAll_btn = document.querySelector(".delete>button")
 
 let library = [];
 
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+class Book {
+    constructor(title, author, pages, status){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
 }
 
 function addToLibrary(book) {
